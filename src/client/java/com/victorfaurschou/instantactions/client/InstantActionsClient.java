894b158c1e0c0b46d76637447286b24f0c0a8bb3
Victor Faurschou <mail@victorfaurschou.com>
2026-06-12 23:49:03 +0200

    Unarchived fabric generated template

---
+++ src/client/java/com/victorfaurschou/instantactions/client/InstantActionsClient.java
@@ -0,0 +1,10 @@
+package com.victorfaurschou.instantactions.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InstantActionsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
